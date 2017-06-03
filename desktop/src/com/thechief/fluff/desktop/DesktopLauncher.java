ce9e4f2c0149ca5d4d892b386def6ff087cbc49e
Ameer Ali <ameergamez06@gmail.com>
2017-06-02 22:29:05 -0400

    Second Commit

---
+++ desktop/src/com/thechief/fluff/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.thechief.fluff.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.thechief.fluff.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
